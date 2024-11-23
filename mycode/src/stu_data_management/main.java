018e03433d17fab907779109bbcebd67d772bda1
RingkhangBTY <ringkhangb913@gmail.com>
2024-11-23 12:38:56 +0530

    Added student data management system

---
+++ mycode/src/stu_data_management/main.java
@@ -0,0 +1,7 @@
+package stu_data_management;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("About to start building before end of this month......");
+    }
+}
